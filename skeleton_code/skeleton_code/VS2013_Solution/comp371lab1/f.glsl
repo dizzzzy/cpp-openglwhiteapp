be25f67cb15d4ca5c337c657e8394e1c6ff0884a
Dimitrios Ziavras <d_ziavra@encs.concordia.ca>
2018-03-15 22:22:27 -0400

    Changed Shininess value

--- skeleton_code/skeleton_code/VS2013_Solution/comp371lab1/f.glsl
+++ skeleton_code/skeleton_code/VS2013_Solution/comp371lab1/f.glsl
@@ -26,7 +26,7 @@ void main()
 	float specularStrength = 0.5;
 	vec3 viewDir = normalize(viewPos - FragPos);
 	vec3 reflectDir = reflect(-lightDir, norm);  
-	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 256);
 	vec3 specular = specularStrength * spec * lightColor;  
 	//0 is ORANGE
 	if(shader_x_pressed == 1){
